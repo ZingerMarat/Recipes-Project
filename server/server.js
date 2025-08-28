21acfd0d63698b681c59f193551319829288f879
Zinger Marat <zingermarat@gmail.com>
2025-08-28 16:29:31 +0300

    feat: add morgan for logging and update package dependencies

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from "express"
 import recipesRouter from "./routes/recipesRouter.js"
+import morgan from "morgan"
 
 const PORT = 3000
+morgan.token("timestamp", () => new Date().toISOString())
 
 const app = express()
+
 app.use(express.json())
+app.use(morgan(":method :url :status - :response-time ms [:timestamp]"))
 
 app.use("/api/recipes", recipesRouter)
 
