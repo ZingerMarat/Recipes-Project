4eb683aaa5a7a63653681f11f4c0354d211dea07
Zinger Marat <zingermarat@gmail.com>
2025-09-04 17:38:42 +0300

    Enhanced Recipe Routes

--- server/controllers/recipesController.js
+++ server/controllers/recipesController.js
@@ -3,8 +3,16 @@ import { readRecipes, writeRecipes } from "../utils/fileHelpers.js"
 import { Op } from "sequelize"
 
 import db from "../db/models/index.js"
-
 const { Recipe, User } = db
+
+import fs from "fs"
+import path from "path"
+import { fileURLToPath } from "url"
+import { dirname } from "path"
+import { log } from "console"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const getAllRecipes = async (req, res, next) => {
   //GET /recipes?difficulty=easy%20medium&maxCookingTime=30&search=pasta%20tomato&sort=rating&order=desc
@@ -59,7 +67,7 @@ export const getRecipe = async (req, res, next) => {
 }
 
 export const addRecipe = async (req, res, next) => {
-  //POST /recipes/
+  //POST /api/recipes (Protected and image upload)
   try {
     const {
       title,
@@ -70,8 +78,6 @@ export const addRecipe = async (req, res, next) => {
       servings,
       difficulty,
       isPublic,
-      //TODO: change hardecoded to req.user.id
-      userId,
     } = req.body
 
     if (!title) {
@@ -88,9 +94,7 @@ export const addRecipe = async (req, res, next) => {
       difficulty,
       isPublic: isPublic !== undefined ? isPublic : true,
       imageUrl: req.file ? `/uploads/recipes/${req.file.filename}` : null,
-      //TODO: change hardecoded to req.user.id
-      //userId: req.user.id,
-      userId,
+      userId: req.userID,
     })
 
     if (!recipe) {
@@ -104,118 +108,182 @@ export const addRecipe = async (req, res, next) => {
 }
 
 export const updateRecipe = async (req, res, next) => {
-  //PUT /recipes/:id
-  const id = req.params.id
-  const update = req.body
-
+  //PUT /api/recipes/:id (Protected)
   try {
-    const recipes = await readRecipes()
+    const {
+      title,
+      description,
+      ingredients,
+      instructions,
+      cookingTime,
+      servings,
+      difficulty,
+      isPublic,
+    } = req.body
 
-    const index = recipes.findIndex((r) => r.id === id)
+    const recipeId = req.params.id
+    const userId = req.userID
 
-    if (index === -1) {
+    //const recipes = await readRecipes()
+    const recipe = await Recipe.findByPk(recipeId)
+
+    if (!recipe) {
       return res.status(404).json({ message: "Recipe not found" })
     }
 
-    recipes[index] = { ...recipes[index], ...update }
+    const plainRecipe = recipe.get({ plain: true })
 
-    await writeRecipes(recipes)
+    if (plainRecipe.userId !== userId) {
+      return res.status(403).json({ message: "You are not authorized to update this recipe" })
+    }
 
-    res.status(200).json(recipes[index])
+    const update = {
+      title,
+      description,
+      ingredients,
+      instructions,
+      cookingTime,
+      servings,
+      difficulty,
+      isPublic,
+      imageUrl: req.file ? `/uploads/recipes/${req.file.filename}` : plainRecipe.imageUrl,
+    }
+
+    await Recipe.update(update, { where: { id: recipeId } })
+
+    const updatedRecipe = await Recipe.findByPk(recipeId)
+    res.status(200).json(updatedRecipe)
   } catch (err) {
     next(err)
   }
 }
 
 export const deleteRecipe = async (req, res, next) => {
-  const id = req.params.id
-
+  //DELETE /api/recipes/:id (Protected)
   try {
-    const recipes = await readRecipes()
+    const recipeId = req.params.id
+    const userId = req.userID
 
-    const index = recipes.findIndex((r) => r.id === id)
-
-    if (index === -1) {
-      return res.status(404).json({ message: "Recipe not found" })
-    }
-
-    recipes.splice(index, 1)
-
-    await writeRecipes(recipes)
-
-    res.status(204).end()
-  } catch (err) {
-    next(err)
-  }
-}
-
-const createStats = (recipes) => {
-  let totalNumber = 0
-  let avgCookingTime = 0
-  let recByDifficulty = { easy: [], hard: [], medium: [] }
-  let mostCommonIng = []
-
-  totalNumber = recipes.length
-
-  avgCookingTime = recipes.reduce((sum, r) => (sum += r.cookingTime), 0) / totalNumber
-
-  recByDifficulty.easy = recipes.filter((r) => r.difficulty === "easy").map((r) => r.id)
-  recByDifficulty.hard = recipes.filter((r) => r.difficulty === "hard").map((r) => r.id)
-  recByDifficulty.medium = recipes.filter((r) => r.difficulty === "medium").map((r) => r.id)
-
-  let allIng = []
-  for (let r of recipes) {
-    allIng.push(...r.ingredients)
-  }
-
-  const ingMap = new Map()
-  for (let ing of allIng) {
-    ingMap.set(ing, (ingMap.get(ing) || 0) + 1)
-  }
-
-  const ingObj = Object.fromEntries(ingMap)
-  const ingArr = Object.entries(ingObj).map(([key, value]) => ({
-    ingredient: key,
-    count: value,
-  }))
-  const sortedIngArr = ingArr.sort((a, b) => b.count - a.count)
-  mostCommonIng = sortedIngArr.slice(0, 10).map((ing) => ing.ingredient)
-
-  return { totalNumber, avgCookingTime, recByDifficulty, mostCommonIng }
-}
-
-export const getStats = async (req, res, next) => {
-  try {
-    const recipes = await readRecipes()
-    const stats = createStats(recipes)
-
-    res.status(200).json(stats)
-  } catch (err) {
-    next(err)
-  }
-}
-
-export const updateRating = async (req, res, next) => {
-  const { id, rating } = req.params
-  const newRating = Number(rating)
-
-  try {
-    if (isNaN(newRating) || newRating < 0 || newRating > 5) {
-      return res.status(400).json({ message: "Rating must be a number between 0 and 5" })
-    }
-
-    const recipes = await readRecipes()
-    const recipe = recipes.find((r) => r.id === id)
+    const recipe = await Recipe.findByPk(recipeId)
 
     if (!recipe) {
       return res.status(404).json({ message: "Recipe not found" })
     }
 
-    recipe.rating = newRating
-    await writeRecipes(recipes)
+    const plainRecipe = recipe.get({ plain: true })
 
-    res.status(200).json(recipe)
+    if (plainRecipe.userId !== userId) {
+      return res.status(403).json({ message: "You are not authorized to delete this recipe" })
+    }
+
+    const imagePath = plainRecipe.imageUrl
+
+    await recipe.destroy()
+
+    if (imagePath) {
+      const fullPath = path.join(__dirname, "../public", imagePath)
+      console.log(fullPath)
+
+      fs.unlink(fullPath, (err) => {
+        if (err) {
+          console.error("Error deleting file:", err)
+        }
+      })
+    }
+
+    res.status(200).json({ message: "Recipe deleted successfully" })
   } catch (err) {
     next(err)
   }
 }
+
+export const getUsersRecipes = async (req, res, next) => {
+  //GET /recipes/my-recipes (Protected)
+  try {
+    const userId = req.userID
+
+    const recipes = await Recipe.findAll({
+      where: { userId },
+      raw: true,
+    })
+
+    if (!recipes.length) {
+      return res.status(404).json({ message: "No recipes found for this user" })
+    }
+
+    res.status(200).json(recipes)
+  } catch (err) {
+    next(err)
+  }
+}
+
+// const createStats = (recipes) => {
+//   let totalNumber = 0
+//   let avgCookingTime = 0
+//   let recByDifficulty = { easy: [], hard: [], medium: [] }
+//   let mostCommonIng = []
+
+//   totalNumber = recipes.length
+
+//   avgCookingTime = recipes.reduce((sum, r) => (sum += r.cookingTime), 0) / totalNumber
+
+//   recByDifficulty.easy = recipes.filter((r) => r.difficulty === "easy").map((r) => r.id)
+//   recByDifficulty.hard = recipes.filter((r) => r.difficulty === "hard").map((r) => r.id)
+//   recByDifficulty.medium = recipes.filter((r) => r.difficulty === "medium").map((r) => r.id)
+
+//   let allIng = []
+//   for (let r of recipes) {
+//     allIng.push(...r.ingredients)
+//   }
+
+//   const ingMap = new Map()
+//   for (let ing of allIng) {
+//     ingMap.set(ing, (ingMap.get(ing) || 0) + 1)
+//   }
+
+//   const ingObj = Object.fromEntries(ingMap)
+//   const ingArr = Object.entries(ingObj).map(([key, value]) => ({
+//     ingredient: key,
+//     count: value,
+//   }))
+//   const sortedIngArr = ingArr.sort((a, b) => b.count - a.count)
+//   mostCommonIng = sortedIngArr.slice(0, 10).map((ing) => ing.ingredient)
+
+//   return { totalNumber, avgCookingTime, recByDifficulty, mostCommonIng }
+// }
+
+// export const getStats = async (req, res, next) => {
+//   try {
+//     const recipes = await readRecipes()
+//     const stats = createStats(recipes)
+
+//     res.status(200).json(stats)
+//   } catch (err) {
+//     next(err)
+//   }
+// }
+
+// export const updateRating = async (req, res, next) => {
+//   const { id, rating } = req.params
+//   const newRating = Number(rating)
+
+//   try {
+//     if (isNaN(newRating) || newRating < 0 || newRating > 5) {
+//       return res.status(400).json({ message: "Rating must be a number between 0 and 5" })
+//     }
+
+//     const recipes = await readRecipes()
+//     const recipe = recipes.find((r) => r.id === id)
+
+//     if (!recipe) {
+//       return res.status(404).json({ message: "Recipe not found" })
+//     }
+
+//     recipe.rating = newRating
+//     await writeRecipes(recipes)
+
+//     res.status(200).json(recipe)
+//   } catch (err) {
+//     next(err)
+//   }
+// }
--- server/routes/recipesRouter.js
+++ server/routes/recipesRouter.js
@@ -6,20 +6,24 @@ import {
   addRecipe,
   updateRecipe,
   deleteRecipe,
-  getStats,
-  updateRating,
+  getUsersRecipes,
 } from "../controllers/recipesController.js"
 import { validateRecipeMiddleware } from "../middleware/validateRecipeMiddleware.js"
 import { upload } from "../middleware/upload.js"
+import { verifyToken } from "../middleware/auth.js"
 
 const router = express.Router()
 
 router.get("/", getAllRecipes)
-router.get("/stats", getStats)
+//router.get("/stats", getStats)
+router.get("/my-recipes", verifyToken, getUsersRecipes)
 router.get("/:id", getRecipe)
-router.post("/", upload.single("image"), validateRecipeMiddleware, addRecipe)
-router.put("/:id", validateRecipeMiddleware, updateRecipe)
-router.put("/:id/rating/:rating", updateRating)
-router.delete("/:id", deleteRecipe)
+
+router.post("/", upload.single("image"), verifyToken, validateRecipeMiddleware, addRecipe)
+
+router.put("/:id", upload.single("image"), verifyToken, validateRecipeMiddleware, updateRecipe)
+//router.put("/:id/rating/:rating", updateRating)
+
+router.delete("/:id", verifyToken, deleteRecipe)
 
 export default router
