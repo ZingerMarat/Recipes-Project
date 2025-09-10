faab13d1099a262fb20f28e40e5cdbb776dacfdd
Zinger Marat <zingermarat@gmail.com>
2025-09-10 23:47:19 +0300

    feat: implement toggleLike functionality to like/unlike comments

--- server/controllers/commentsController.js
+++ server/controllers/commentsController.js
@@ -22,3 +22,35 @@ export const editComment = async (req, res, next) => {
     next(err)
   }
 }
+
+export const toggleLike = async (req, res, next) => {
+  try {
+    const commentId = req.params.commentId
+    const userId = req.userID
+
+    const comment = await Comment.findById(commentId)
+
+    if (!comment) {
+      return res.status(404).json({ success: false, message: "Comment not found" })
+    }
+
+    let updated
+    if (comment.likes.includes(userId)) {
+      updated = await Comment.findByIdAndUpdate(
+        commentId,
+        { $pull: { likes: userId } },
+        { new: true }
+      )
+    } else {
+      updated = await Comment.findByIdAndUpdate(
+        commentId,
+        { $addToSet: { likes: userId } },
+        { new: true }
+      )
+    }
+
+    res.status(200).json(updated)
+  } catch (err) {
+    next(err)
+  }
+}
--- server/routes/commentsRouter.js
+++ server/routes/commentsRouter.js
@@ -1,9 +1,11 @@
 import express from "express"
 
 import { verifyToken } from "../middleware/auth.js"
-import { editComment } from "../controllers/commentsController.js"
+import { editComment, toggleLike } from "../controllers/commentsController.js"
 
 const router = express.Router()
+
+router.post("/:commentId/like", verifyToken, toggleLike)
 
 router.put("/:commentId", verifyToken, editComment)
 
