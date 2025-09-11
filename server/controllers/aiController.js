a3429192f013c6ef76b45f5ff814e14d906e5ce7
Zinger Marat <zingermarat@gmail.com>
2025-09-11 16:09:05 +0300

    feat: add ingredient substitution functionality with new controller and router

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,4 +1,6 @@
 import { GoogleGenAI, Type } from "@google/genai"
+import db from "../db/models/index.js"
+const { Recipe } = db
 
 import dotenv from "dotenv"
 dotenv.config()
@@ -7,7 +9,7 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_GEMINI_API_KEY,
 })
 
-const config = {
+const recipeConfig = {
   responseMimeType: "application/json",
   responseSchema: {
     type: Type.OBJECT,
@@ -23,6 +25,27 @@ const config = {
             confidence: { type: Type.NUMBER },
           },
           propertyOrdering: ["title", "ingredients", "instructions", "confidence"],
+        },
+      },
+    },
+  },
+}
+
+const ingredientConfig = {
+  responseMimeType: "application/json",
+  responseSchema: {
+    type: Type.OBJECT,
+    properties: {
+      substitutions: {
+        type: Type.ARRAY,
+        items: {
+          type: Type.OBJECT,
+          properties: {
+            substitute: { type: Type.STRING },
+            ratio: { type: Type.STRING },
+            notes: { type: Type.STRING },
+          },
+          propertyOrdering: ["substitute", "ratio", "notes"],
         },
       },
     },
@@ -45,7 +68,48 @@ export const getRecipeSuggestions = async (req, res, next) => {
 
     const response = await ai.models.generateContent({
       model,
-      config,
+      config: recipeConfig,
+      contents: [{ role: "user", parts: [{ text: prompt }] }],
+    })
+
+    const answer = JSON.parse(response?.text)
+
+    res.status(200).send(answer)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getIngredientSubstitutions = async (req, res, next) => {
+  try {
+    const { recipe_id, substitute_ingredient, dietary_need } = req.body
+
+    const recipe = await Recipe.findByPk(recipe_id)
+
+    if (!recipe) {
+      return res.status(404).json({ message: "No recipe found with the same id" })
+    }
+
+    const ingredients = recipe.ingredients
+    const instructions = recipe.instructions
+
+    const prompt = `
+                    You are given a recipe (ingredients + instructions).
+
+                    Suggest **3** possible substitutions for only this ingredient**: ${substitute_ingredient}
+
+                    Conditions:
+                    - Consider how the substitution affects the rest of the recipe.
+                    - All other ingredients must stay unchanged.
+                    - Adapt notes and ratio if needed.
+                    - Explore these ingredients: ${ingredients}
+                    - Explore instructions: ${instructions}
+                    - Important information about diet: ${dietary_need}
+                    `
+
+    const response = await ai.models.generateContent({
+      model,
+      config: ingredientConfig,
       contents: [{ role: "user", parts: [{ text: prompt }] }],
     })
 
--- server/routes/aiRouter.js
+++ server/routes/aiRouter.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { verifyToken } from "../middleware/auth.js"
-import { getRecipeSuggestions } from "../controllers/aiController.js"
+import { getRecipeSuggestions, getIngredientSubstitutions } from "../controllers/aiController.js"
 
 const router = express.Router()
 
 router.post("/recipe-suggections", verifyToken, getRecipeSuggestions)
+router.post("/ingredient-substitutions", verifyToken, getIngredientSubstitutions)
 
 export default router
