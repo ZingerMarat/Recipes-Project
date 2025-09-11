d8e56c4b484c80c207f8013e7ab9f576f2e9d3ec
Zinger Marat <zingermarat@gmail.com>
2025-09-11 11:24:27 +0300

    feat: implement addToCollection functionality and update getCollections route

--- server/controllers/collectionsController.js
+++ server/controllers/collectionsController.js
@@ -37,3 +37,54 @@ export const addCollection = async (req, res, next) => {
     next(err)
   }
 }
+
+export const getCollections = async (req, res, next) => {
+  try {
+    const userId = req.userID
+
+    const collections = await RecipeCollection.find({ $or: [{ userId }, { isPublic: true }] })
+
+    if (!collections.length) {
+      return res.status(404).json({ message: "No collection found" })
+    }
+
+    res.status(200).json(collections)
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const addToCollection = async (req, res, next) => {
+  try {
+    const collectionId = req.params.id
+    const { recipeId, notes } = req.body
+
+    const exists = await RecipeCollection.findOne({
+      _id: collectionId,
+      "recipes.recipeId": recipeId,
+    })
+
+    if (exists) return res.status(400).json({ message: "Recipe already in collection" })
+
+    const recipe = await Recipe.findByPk(recipeId)
+    if (!recipe) {
+      return res.status(404).json({ message: "No recipe found with this id" })
+    }
+
+    const recipeTitle = recipe.title
+
+    const addedRecipe = await RecipeCollection.findByIdAndUpdate(
+      collectionId,
+      {
+        $addToSet: {
+          recipes: { recipeId, recipeTitle, notes },
+        },
+      },
+      { new: true }
+    )
+
+    res.status(201).json(addedRecipe)
+  } catch (err) {
+    next(err)
+  }
+}
--- server/routes/collectionsRouter.js
+++ server/routes/collectionsRouter.js
@@ -1,11 +1,15 @@
 import express from "express"
 import { verifyToken } from "../middleware/auth.js"
-import { addCollection } from "../controllers/collectionsController.js"
+import {
+  addCollection,
+  addToCollection,
+  getCollections,
+} from "../controllers/collectionsController.js"
 
 const router = express.Router()
 
-//router.get("/")
+router.get("/", verifyToken, getCollections)
 router.post("/", verifyToken, addCollection)
-//router.post("/:id/recipes")
+router.post("/:id/recipes", verifyToken, addToCollection)
 
 export default router
